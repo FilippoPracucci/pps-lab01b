fe7d1546fecb67f45430b40e49abf91577b6f452
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-03-04 18:30:50 +0100

    [ex2-step1] implemented some tests for LogicsImpl

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,15 +1,34 @@
 package e2;
+
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+    private static final int BOARD_SIZE = 5;
+    private Logics logics;
+
+    @BeforeEach
+    void init() {
+        this.logics = new LogicsImpl(BOARD_SIZE);
+    }
+
+    @Test
+    public void testIfPawnHasBeenHit() {
+        final int rowToHit = 2;
+        final int columnToHit = 2;
+        final boolean isPawInPositionToHit = this.logics.hasPawn(rowToHit, columnToHit);
+        assertEquals(isPawInPositionToHit, this.logics.hit(rowToHit, columnToHit));
+    }
+
+    @Test
+    public void testKnightMovementOutOfBounds() {
+        final int rowOutOfBounds = -1;
+        final int columnOutOfBounds = -1;
+        assertAll(
+            () -> assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(BOARD_SIZE, BOARD_SIZE)),
+            () -> assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(rowOutOfBounds, columnOutOfBounds))
+        );
+    }
+
 }
